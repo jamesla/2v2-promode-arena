7817a3990e1d4f9c4e8f8f8bcdbb5bc248021770
root <root@ra3arena.ra3arena.b7.internal.cloudapp.net>
2016-05-09 11:42:25 +0000

    updates to deploy script add workshop ra3 content

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker rm -f /redis
+docker rm -f ql
 docker build -t quakelive .
 docker run -d --name redis -v ql-redis:/data redis
-docker run -p 27960:27960/udp --link redis -d --name ql -e name="Test Server" -e admin="12345" quakelive
+docker run -p 27960:27960/udp --link redis -d --name ql quakelive
