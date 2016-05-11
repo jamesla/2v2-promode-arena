922c80181f248777a0de1ac46e4e3bcf039a5b60
root <root@ra3arena.ra3arena.b7.internal.cloudapp.net>
2016-05-11 06:31:35 +0000

    update docker deploy

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -4,4 +4,4 @@ docker rm -f /redis
 docker rm -f ql
 docker build -t quakelive .
 docker run -d --name redis -v ql-redis:/data redis
-docker run -p 27960:27960/udp --link redis -d --name ql quakelive
+docker run -p 27960:27960/udp --link redis -d --name ql -e name="Test Server James" -e admin="76561197999716762" quakelive
