15d083955918916c9abbdec1d06a202514931c75
root <root@ra32v2.ra32v2.p1.internal.cloudapp.net>
2016-05-17 11:20:40 +0000

    add maps

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -4,4 +4,4 @@ docker rm -f /redis
 docker rm -f ql
 docker build -t quakelive .
 docker run -d --name redis -v ql-redis:/data redis
-docker run -p 27960:27960/udp --link redis -d --name ql -e name="2v2 Promode on oldschool ra3 maps" -e admin="76561197999716762" quakelive
+docker run -p 27960:27960/udp --link redis -d --name ql -e name="2v2 hardouts only (old school maps" -e admin="76561197961679861" quakelive
